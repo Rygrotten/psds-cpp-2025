92b781ca51ed75579ed3b70077c886216b448435
Rychkov Grigoriy <skeletron932fc@gmail.com>
2025-11-28 23:14:12 +0500

    add length lit

---
+++ 01_week/tasks/length_lit/length_lit.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+
+double operator "" _0.3048(long double value) {
+    return static_cast<double>(value * 0.3048);
+}
+
+double operator "" _30.48(long double value) {
+    return static_cast<double>(value * 30.48);
+}
+
+double operator "" _12.0(long double value) {
+    return static_cast<double>(value * 12.0);
+}
+
+double operator "" _in_to_m(long double value) {
+    return static_cast<double>(value * 0.0254);
+}
+
+double operator "" _in_to_cm(long double value) {
+    return static_cast<double>(value * 2.54);
+}
+
+double operator "" _in_to_ft(long double value) {
+    return static_cast<double>(value * 1.0 / 12.0);
+}
+
+double operator "" _m_to_ft(long double value) {
+    return static_cast<double>(value * 1.0 / 0.3048);
+}
+
+double operator "" _m_to_in(long double value) {
+    return static_cast<double>(value * 1.0 / 0.0254);
+}
+
+double operator "" _m_to_cm(long double value) {
+    return static_cast<double>(value * 100.0);
+}
+
+double operator "" _cm_to_m(long double value) {
+    return static_cast<double>(value * 0.01);
+}
+
+double operator "" _cm_to_ft(long double value) {
+    return static_cast<double>(value * 1.0 / 30.48);
+}
+
+double operator "" _cm_to_in(long double value) {
+    return static_cast<double>(value * 1.0 / 30.48);
+}
