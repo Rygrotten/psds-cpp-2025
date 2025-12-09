4e610a4727d60e34c8996a5a9d2a8053b087eb80
Rychkov Grigoriy <skeletron932fc@gmail.com>
2025-12-09 22:50:31 +0500

    pretty_array attempt 1

--- 02_week/tasks/pretty_array/pretty_array.cpp
+++ 02_week/tasks/pretty_array/pretty_array.cpp
@@ -1,6 +1,50 @@
-#include <stdexcept>
+#include <iostream>
 
-
-void PrintArray(/* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+void PrintArray(const int* begin, const int* end, size_t limit = 0) {
+    if (begin == end) {
+        std::cout << "[]\n";
+        return;
+    }
+    
+    bool forward = (end > begin);
+    
+    const int* current = begin;
+    
+    std::cout << '[';
+    
+    size_t count_in_line = 0;
+    
+    bool first_element = true;
+    
+    while (true) {
+        if (!first_element) {
+            std::cout << ", ";
+        }
+        std::cout << *current;
+        first_element = false;
+        
+        count_in_line++;
+        
+        const int* next = nullptr;
+        if (forward) {
+            next = current + 1;
+        } else {
+            next = current - 1;
+        }
+        
+        bool has_more_elements = (forward && next < end) || (!forward && next > end);
+        
+        if (limit > 0 && count_in_line >= limit && has_more_elements) {
+            std::cout << ", ...\n ";
+            count_in_line = 0;
+        }
+        
+        if (!has_more_elements) {
+            break;
+        }
+        
+        current = next;
+    }
+    
+    std::cout << "]\n";
 }
