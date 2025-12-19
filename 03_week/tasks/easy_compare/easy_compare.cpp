04b641b5a14ffd350f48e244f4bf1dfb4d05eb53
Rychkov Grigoriy <skeletron932fc@gmail.com>
2025-12-19 21:37:50 +0500

    EasyCompare 1

--- 03_week/tasks/easy_compare/easy_compare.cpp
+++ 03_week/tasks/easy_compare/easy_compare.cpp
@@ -1,5 +1,5 @@
 #include <stdexcept>
-
+#include <tuple>
 
 struct Date {
     unsigned year;
@@ -14,3 +14,63 @@ struct StudentInfo {
     unsigned course;
     Date birth_date;
 };
+
+bool operator==(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) == 
+           std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator!=(const Date& lhs, const Date& rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator<(const Date& lhs, const Date& rhs) {
+    return std::tie(lhs.year, lhs.month, lhs.day) < 
+           std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator<=(const Date& lhs, const Date& rhs) {
+    return !(rhs < lhs);
+}
+
+bool operator>(const Date& lhs, const Date& rhs) {
+    return rhs < lhs;
+}
+
+bool operator>=(const Date& lhs, const Date& rhs) {
+    return !(lhs < rhs);
+}
+
+int mark_priority(char mark) {
+    switch (mark) {
+        case 'A': return 5;
+        case 'B': return 4;
+        case 'C': return 3;
+        case 'D': return 2;
+        case 'Z': return 1;
+        default: return 0;     }
+}
+
+bool operator==(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return lhs.mark == rhs.mark && lhs.score == rhs.score;
+}
+
+bool operator!=(const StudentInfo& lhs, const StudentInfo& rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator<(const StudentInfo& lhs, const StudentInfo& rhs) {
+    int lhs_priority = mark_priority(lhs.mark);
+    int rhs_priority = mark_priority(rhs.mark);
+    
+    if (lhs_priority != rhs_priority) {
+        return lhs_priority < rhs_priority;     }
+
+    if (lhs.score != rhs.score) {
+        return lhs.score < rhs.score; // Меньший балл = хуже
+    }
+    
+        if (lhs.course != rhs.course) {
+                return lhs.course > rhs.course;     }
+    
+        return lhs.birth_date < rhs.birth_date; }
